d5314332d4d68862da5a447faf8d26cef511f685
Alan <aln0071@gmail.com>
2019-10-14 13:36:25 +0530

    replaced js with jquery

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-// document.getElementById('feature').addEventListener('click', function() {
-// 	document.getElementById('featurelink').click();
-// 	let height = document.documentElement.clientHeight - $('#featureiframe').offset().top - 20;
-// 	$('#featureiframe').height(height);
-// })
-
  document.getElementById('feature').addEventListener('click', function() {
-	document.getElementById('featurelink').click();
+	$('.nav-tabs a[href="#menu1"]').tab('show')
 });
 
 $('.nav-tabs a[href="#menu1"]').on('shown.bs.tab', function(event){
