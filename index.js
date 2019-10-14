dc8bc49d3ea111e40542beb8d8c2e9dc03dd0035
Alan <aln0071@gmail.com>
2019-10-14 13:46:23 +0530

    resize iframe on window resize added

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
- document.getElementById('feature').addEventListener('click', function() {
+
+var currentTab = '';
+
+document.getElementById('feature').addEventListener('click', function() {
 	$('.nav-tabs a[href="#menu1"]').tab('show')
 });
 
-$('.nav-tabs a[href="#menu1"]').on('shown.bs.tab', function(event){
+function resetFeatureIframeHeight() {
 	let height = document.documentElement.clientHeight - $('#featureiframe').offset().top - 20;
 	$('#featureiframe').height(height);
+}
+
+$('.nav-tabs a[href="#menu1"]').on('shown.bs.tab', function(event){
+	resetFeatureIframeHeight();
+});
+
+$(window).resize(function () {
+	if(currentTab === 'Feature to JSON')
+		resetFeatureIframeHeight();
+});
+
+$('.nav-tabs a').on('shown.bs.tab', function(event){
+  currentTab = $(event.target).text();         // active tab
 });
