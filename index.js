072f408d38136f7d84523ccdb6037a0142070ee0
Alan <aln0071@gmail.com>
2019-10-14 13:31:58 +0530

    styles changed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("hello");
+// document.getElementById('feature').addEventListener('click', function() {
+// 	document.getElementById('featurelink').click();
+// 	let height = document.documentElement.clientHeight - $('#featureiframe').offset().top - 20;
+// 	$('#featureiframe').height(height);
+// })
+
+ document.getElementById('feature').addEventListener('click', function() {
+	document.getElementById('featurelink').click();
+});
+
+$('.nav-tabs a[href="#menu1"]').on('shown.bs.tab', function(event){
+	let height = document.documentElement.clientHeight - $('#featureiframe').offset().top - 20;
+	$('#featureiframe').height(height);
+});
