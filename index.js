a652a476628913cc77ec147360b8b1858133a136
Alan Kuriakose <Alan.Kuriakose.contractor@hilton.com>
2020-03-10 10:10:42 +0530

    added ontime tracker link to the jumbotron and prevented event propagation on mailto link

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 
 var currentTab = '';
 
+// event listener to handle click on feature to json jombotron
 document.getElementById('feature').addEventListener('click', function() {
 	$('.nav-tabs a[href="#menu1"]').tab('show')
 });
+
+// event listener to handle click on ontime tracker jombotron
+document.getElementById('ontimetracker').addEventListener('click', function() {
+	window.open("https://chrome.google.com/webstore/detail/ontime-tracker/geihfdjjmcldhcdgfipoodbniaepkfai", '_blank');
+})
+
+// prevent event propogation on mailto anchor tag
+$('#mailto').click((e)=>{
+	e.stopPropagation();
+})
 
 function resetFeatureIframeHeight() {
 	let height = document.documentElement.clientHeight - $('#featureiframe').offset().top - 20;
